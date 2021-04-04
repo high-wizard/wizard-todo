35c57caf2f4d4c8b2c246805100cad29459ae54c
Wizard-kuramoto <72068447+Wizard-kuramoto@users.noreply.github.com>
2021-04-05 08:47:49 +0900

    簡易的なアニメーションを追加
    リストの追加時と削除時をフワッとさせました。

--- src/App.js
+++ src/App.js
@@ -1,86 +1,113 @@
-import React, {useState} from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
-import Container from './components/Container'
-import ListHeader from './components/ListHeader'
-import List from './components/List'
-import SideButtons from './components/SideButtons'
+import Container from "./components/Container";
+import ListHeader from "./components/ListHeader";
+import List from "./components/List";
+import SideButtons from "./components/SideButtons";
 
 // todoを特定するための一意なキーに使用する
-let currentId = 0
+let currentId = 0;
 
 function App() {
   // todoリストのデータが入るstate
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
   // todoリスト自身が入るstate
-  const [text, setText] = useState('')
+  const [text, setText] = useState("");
 
   const onChangeHandler = (e) => {
-    setText(e.target.value)
-  }
+    setText(e.target.value);
+  };
 
   // 新しいtodoを追加する関数
   const pushTodo = (e) => {
     // イベントのデフォルト動作を発生させない
-    e.preventDefault()
-    if(!text) {
-      return
+    e.preventDefault();
+    if (!text) {
+      return;
     }
 
     // 新しいTodoの作成
     const newTodo = {
       id: currentId,
       checked: false,
-      text: text
-    }
+      text: text,
+      isActive: true,
+    };
 
     //　入力欄と対応するstateをリセットする
-    setText('')
+    setText("");
     // 今までのtodosと新しく作ったtodoを合成する
-    setTodos([...todos, newTodo])
+    setTodos([...todos, newTodo]);
     // 次のオリジナルキーが重複しないようにインクリメントする
-    currentId += 1
-  }
+    currentId += 1;
+  };
 
   // checkboxのアクティブをトグルさせる関数
   const toggleActive = (id) => {
     // todosからidが一致したtodoのアクティブ状態だけを反転させて返す
-    const filtedTodos = todos.map(todo => {
-      if(todo.id === id) {
+    const filtedTodos = todos.map((todo) => {
+      if (todo.id === id) {
         // todoのcheckedを反転して上書きする
-        return  {
+        return {
           ...todo,
-          checked: !todo.checked
-        }
+          checked: !todo.checked,
+        };
       }
-      return todo
-    })
+      return todo;
+    });
 
     // 新しいtodosをセットする
-    setTodos(filtedTodos)
+    setTodos(filtedTodos);
+  };
+
+  // 削除前にアニメーションフラグを切り替える
+  const disableTodos = () => {
+    // 削除予定のtodoを非アクティブ化
+    const disabledTodos = todos.map((todo) => {
+      if(todo.checked) {
+        return {
+          ...todo,
+          isActive: false,
+        };
+      }
+      // 削除予定でないものはノータッチ
+      return todo
+    });
+
+    // 処理済みのtodosを返す
+    return disabledTodos
   }
 
   // checkboxがアクティブの要素だけ全て削除する
   const removeCheckedTodos = () => {
-    // checkboxがアクティブの要素以外のtodosのコピーを返す
-    const filtedTodos = todos.filter(todo => !todo.checked)
-    // 新しいtodosをセットする
-    setTodos(filtedTodos)
-  }
+    // アニメーション終了を待って削除
+    setTimeout(() => {
+      // checkboxがアクティブの要素以外のtodosのコピーを返す
+      const filtedTodos = todos.filter((todo) => !todo.checked);
+      // 新しいtodosをセットする
+      setTodos(filtedTodos);
+    }, 300);
+
+    // 削除予定のtodosからアニメーションのためのフラグを切り替える
+    const disabledTodos = disableTodos()
+    // 新しいtodosをセットする(削除処理はここでは関係なし)
+    setTodos(disabledTodos)
+  };
 
   return (
     <div className="App">
       <Container>
         <h1 className="title">Example Todo</h1>
         <div className="content">
-          <SideButtons removeCheckedTodos={removeCheckedTodos}/>
+          <SideButtons removeCheckedTodos={removeCheckedTodos} />
           <div className="list">
             <ListHeader
               text={text}
               onChangeHandler={onChangeHandler}
               pushTodo={pushTodo}
             />
-            <List todos={todos} toggleActive={toggleActive}/>
+            <List todos={todos} toggleActive={toggleActive} />
           </div>
         </div>
       </Container>
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,29 +1,39 @@
-import React from 'react'
-import {motion} from 'framer-motion';
-import '../styles/ListItem.css'
+import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import "../styles/ListItem.css";
 
-const ListItem = ({todo, toggleActive, item}) => {
-
+const ListItem = ({ todo, toggleActive }) => {
   const truncate = (str, n) => {
-    return str && str.length > n ? str.substr(0, n -1) + "..." : str;
-  }
+    return str && str.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
 
-  const labelStyle = `list-item-label ${todo.checked ? "list-item-active" : ""}`
+  const labelStyle = `list-item-label ${
+    todo.checked ? "list-item-active" : ""
+  }`;
 
   return (
-    <motion.li className="list-item" variants={item}>
-      <input
-        className="list-item-input"
-        type="checkbox"
-        id={todo.id}
-        checked={todo.checked}
-        onChange={() => toggleActive(todo.id)}
-      />
-      <label className={labelStyle} htmlFor={todo.id}>
-        {truncate(todo.text, 30)}
-      </label>
-    </motion.li>
-  )
-}
+    <AnimatePresence>
+      {todo.isActive &&
+        <motion.li
+          className="list-item"
+          initial={{ opacity: 0, y: "20px" }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: "20px" }}
+          transition={{ duration: .3 }}
+        >
+          <input
+            className="list-item-input"
+            type="checkbox"
+            id={todo.id}
+            checked={todo.checked}
+            onChange={() => toggleActive(todo.id)}
+          />
+          <label className={labelStyle} htmlFor={todo.id}>
+            {truncate(todo.text, 30)}
+          </label>
+        </motion.li>}
+    </AnimatePresence>
+  );
+};
 
-export default ListItem
+export default ListItem;
