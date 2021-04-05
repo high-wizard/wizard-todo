d0da74f744905e0ab80f1be814f4a75e8aa14158
Yudai Yamamoto <yudai.yamamoto@high-wizard.com>
2021-04-05 18:23:53 +0900

    トグルボタンの追加

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {AnimatePresence, motion} from 'framer-motion'
 import '../styles/ListItem.css'
 
 const ListItem = ({todo, toggleActive}) => {
@@ -13,14 +12,10 @@ const ListItem = ({todo, toggleActive}) => {
   }`;
 
   return (
-    <AnimatePresence>
+    <ul>
       {todo.isActive &&
-        <motion.li 
+        <li 
           className="list-item" 
-          initial={{ opaciity: 0, y: "20px"}}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{opacity: 0, y:"20px"}}
-          transition={{ duration: .3}}
         >
         <input
           className="list-item-input"
@@ -32,9 +27,9 @@ const ListItem = ({todo, toggleActive}) => {
         <label className={labelStyle}>
           {truncate(todo.text, 30)}
         </label>
-      </motion.li>
+      </li>
       }
-    </AnimatePresence>
+    </ul>
   )
 }
 
